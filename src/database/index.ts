842d6b1eea0af574c8c582f0ca7e6ea59dade0e5
argonmining <ashton@onargon.com>
2024-08-12 12:29:55 -0500

    updated database index.ts from Client to Pooled Connections to avoid oversaturating the db with connections

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 
 type Miner = {
   balance: bigint;
@@ -11,32 +11,41 @@ type MinerBalanceRow = {
 };
 
 export default class Database {
-  client: Client;
+  private pool: Pool;
 
   constructor(connectionString: string) {
-    this.client = new Client({
+    this.pool = new Pool({
       connectionString: connectionString,
     });
-    this.client.connect();
   }
 
   async getAllBalancesExcludingPool() {
-    const res = await this.client.query(
-      `SELECT wallet, SUM(balance) as total_balance 
-       FROM miners_balance 
-       WHERE miner_id != $1 
-       GROUP BY wallet`,
-      ['pool']
-    );
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query(
+        `SELECT wallet, SUM(balance) as total_balance 
+         FROM miners_balance 
+         WHERE miner_id != $1 
+         GROUP BY wallet`,
+        ['pool']
+      );
 
-    return res.rows.map((row: { wallet: string, total_balance: string }) => ({
-      minerId: 'aggregate', // Placeholder ID for aggregated balance, since we're grouping by wallet.
-      address: row.wallet,
-      balance: BigInt(row.total_balance)
-    }));
+      return res.rows.map((row: { wallet: string, total_balance: string }) => ({
+        minerId: 'aggregate', // Placeholder ID for aggregated balance, since we're grouping by wallet.
+        address: row.wallet,
+        balance: BigInt(row.total_balance)
+      }));
+    } finally {
+      client.release();
+    }
   }
 
   async resetBalancesByWallet(wallet: string) {
-    await this.client.query('UPDATE miners_balance SET balance = $1 WHERE wallet = $2', [0n, wallet]);
+    const client = await this.pool.connect();
+    try {
+      await client.query('UPDATE miners_balance SET balance = $1 WHERE wallet = $2', [0n, wallet]);
+    } finally {
+      client.release();
+    }
   }
 }
