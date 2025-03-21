ee3d43256b286d606f9ac030dcdf15592cc81aae
kr-kashyap <kashyap.n@knackroot.com>
2025-03-21 15:49:43 +0530

    Fix logs and indentation. Improvised msg.

--- src/alerting/telegramBot.ts
+++ src/alerting/telegramBot.ts
@@ -38,44 +38,43 @@ export class TelegramBotAlert {
         let poolBalance = 0n;
   
         try {
-          // Fetch KAS balance
-          treasuryKASBalance  = await fetchKASBalance(transactionManager!.address);
-          monitoring.debug(`Main: KAS balance at alert schedule: ${sompiToKAS(Number(treasuryKASBalance))} KAS`);
+            // Fetch KAS balance
+            treasuryKASBalance  = await fetchKASBalance(transactionManager!.address);
+            monitoring.debug(`TelegramBotAlert: KAS balance at alert schedule: ${sompiToKAS(Number(treasuryKASBalance))} KAS`);
         } catch (error) {
-  
+            monitoring.error(`TelegramBotAlert: Can not fetch treasury KAS balance: ${error}.`);
         }
   
         try {
-          // Fetch NACHO balance
-          treasuryNACHOBalance = await krc20Token(transactionManager!.address, config.defaultTicker);
-          monitoring.debug(`Main: ${config.defaultTicker} balance at alert schedule: ${sompiToKAS(Number(treasuryNACHOBalance))} ${config.defaultTicker}`);
+            // Fetch NACHO balance
+            treasuryNACHOBalance = await krc20Token(transactionManager!.address, config.defaultTicker);
+            monitoring.debug(`TelegramBotAlert: ${config.defaultTicker} balance at alert schedule: ${sompiToKAS(Number(treasuryNACHOBalance))} ${config.defaultTicker}`);
         } catch (error) {
-  
+            monitoring.error(`TelegramBotAlert: Can not fetch treasury NACHO balance: ${error}.`);
         }
   
         try {
-          // Fetch outstanding KAS to be paid
-          totalEligibleAmount = await transactionManager!.db.getAllPendingBalanceAboveThreshold(Number(config.thresholdAmount));
+            // Fetch outstanding KAS to be paid
+            totalEligibleAmount = await transactionManager!.db.getAllPendingBalanceAboveThreshold(Number(config.thresholdAmount));
         } catch (error) {
-  
+            monitoring.error(`TelegramBotAlert: Can not fetch total eligible KAS value: ${error}.`);
         }
   
         try {
-            
             // Fetch pool balance
             poolBalance = 0n;
             if (poolBalances.length > 0) {
                 poolBalance = BigInt(poolBalances[0].balance);
             } else {
-                transactionManager!.monitoring.error("Main: Could not fetch Pool balance from Database.")
+                transactionManager!.monitoring.error("TelegramBotAlert: Could not fetch Pool balance from Database.")
             }
             
             // Fetch outstanding NACHO eligible payment
             const swapToKrc20Obj = new swapToKrc20();
             totalKASWorthNACHO = await swapToKrc20Obj!.swapKaspaToKRC(poolBalance);
-            monitoring.debug(`Main: Amount of ${config.defaultTicker} tokens to be used for NACHO rebate: ${sompiToKAS(Number(totalKASWorthNACHO))} ${config.defaultTicker}`); 
+            monitoring.debug(`TelegramBotAlert: Amount of ${config.defaultTicker} tokens to be used for NACHO rebate: ${sompiToKAS(Number(totalKASWorthNACHO))} ${config.defaultTicker}`); 
         } catch (error) {
-            
+            monitoring.error(`TelegramBotAlert: Can not determine tokens to be used for NACHO rebate: ${error}.`);
         }
   
         let msg = `<b>Wallet Address:</b> <a href="${url}">View pool treasury</a>\n`;
@@ -83,9 +82,13 @@ export class TelegramBotAlert {
             msg += `\n<b>KAS Balance</b>\n--------------------------\n`
             msg += `<b>Current:</b> <code>${sompiToKAS(treasuryKASBalance)} KAS</code>.\n`;
             msg += `<b>Threshold:</b> <code>${sompiToKAS(Number(config.kasAlertThreshold))} KAS</code>.\n`;
-            if (treasuryKASBalance <= totalEligibleAmount) {
-                msg += `<b>Total eligible:</b> <code>${sompiToKAS(Number(totalEligibleAmount))} KAS</code>.\n`;
+        }
+        if (treasuryKASBalance <= totalEligibleAmount) {
+            if (msg.search('KAS Balance') === -1) {
+                msg += `\n<b>KAS Balance</b>\n--------------------------\n`;
+                msg += `<b>Current:</b> <code>${sompiToKAS(treasuryKASBalance)} KAS</code>.\n`;
             }
+            msg += `<b>Total eligible:</b> <code>${sompiToKAS(Number(totalEligibleAmount))} KAS</code>.\n`;
         }
   
         if (treasuryNACHOBalance <= Number(config.nachoAlertThreshold)) {
@@ -93,9 +96,13 @@ export class TelegramBotAlert {
             if (msg != '') msg += `\n`;
             msg += `<b>Current:</b> <code>${sompiToKAS(treasuryNACHOBalance)} NACHO</code>.\n`;
             msg += `<b>Threshold:</b> <code>${sompiToKAS(Number(config.nachoAlertThreshold))} NACHO</code>.\n`;
-            if (treasuryNACHOBalance <= totalKASWorthNACHO) {
-                msg += `<b>Total outstanding KAS worth NACHO</b> <code>${sompiToKAS(Number(totalKASWorthNACHO))} NACHO</code>.\n`;
+        }
+        if (treasuryNACHOBalance <= totalKASWorthNACHO) {
+            if (msg.search('NACHO Balance') === -1) {
+                msg += `\n<b>NACHO Balance</b>\n--------------------------\n`;
+                msg += `<b>Current:</b> <code>${sompiToKAS(treasuryNACHOBalance)} NACHO</code>.\n`;
             }
+                msg += `<b>Total outstanding KAS worth NACHO</b> <code>${sompiToKAS(Number(totalKASWorthNACHO))} NACHO</code>.\n`;
         }
       
         return msg;
