df601291881bbb7458dd511ef0f1e14878d2957b
kr-kashyap <kashyap.n@knackroot.com>
2025-04-01 15:32:45 +0530

    Update default value for alert cron.

--- src/cron-schedule.ts
+++ src/cron-schedule.ts
@@ -13,7 +13,7 @@ export function cronValidation(expr: string, alert = false) {
             return "0 */12 * * *";
         } else {
             monitoring.error('cron-schedule: Invalid cron expression for alerting. Defaulting to Four times a day.');
-            return "0 */6 * * *";
+            return "0 1,7,13,19 * * *";
         }
     }    
 }
