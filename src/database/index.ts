e44183fcc1389ac7f5740f0b7fa58e0936aebaf1
kr-kashyap <kashyap.n@knackroot.com>
2025-04-11 12:29:06 +0530

    Remove extra space.

--- src/database/index.ts
+++ src/database/index.ts
@@ -198,7 +198,7 @@ export default class Database {
     
       monitoring.debug(`database: updatePendingKRC20TransferStatus - query will be invoked for ${p2shAddr}`);
       await this.pool.query(query, [updatedStatus, p2shAddr]);
-      monitoring.debug(`database: updatePendingKRC20TransferStatus - query executed for ${p2shAddr}`);      
+      monitoring.debug(`database: updatePendingKRC20TransferStatus - query executed for ${p2shAddr}`);
     } catch (error) {
       monitoring.error(`database: updating pending KRC20 transfer: ${error}`);      
       throw error;
