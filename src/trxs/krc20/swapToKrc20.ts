53f12e4f276cfb8d4a01dd1026b87f582fa12a91
kr-kashyap <kashyap.n@knackroot.com>
2025-04-25 15:18:36 +0530

    Add headers for fixing API request with 403 errors.

--- src/trxs/krc20/swapToKrc20.ts
+++ src/trxs/krc20/swapToKrc20.ts
@@ -35,7 +35,22 @@ export default class swapToKrc20 {
      * @returns The equivalent amount of NACHO if a swap is possible; otherwise, an indication that no swap occurred.  
      */ 
     async swapKaspaToKRC(balance: bigint) {        
-        const response = await axios.get(quoteURL);
+        const response = await axios.get(quoteURL, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5', 
+                'Accept-Encoding': 'gzip, deflate, br, zstd', 
+                'Connection': 'keep-alive', 
+                'Upgrade-Insecure-Requests': '1', 
+                'Sec-Fetch-Dest': 'document', 
+                'Sec-Fetch-Mode': 'navigate', 
+                'Sec-Fetch-Site': 'none', 
+                'Sec-Fetch-User': '?1', 
+                'If-None-Match': 'W/"39-vzuMegLlGZSEPyz6L6YgMFU3WmI"',
+                'Priority': 'u=0, i'
+            }
+        });
         
         const floorPrice = Number(response.data[0]?.floor_price);
         monitoring.log(`swapKaspaToKRC: floor price ${floorPrice}`);
